46de31cf09d0f148d88692c9be61c1e995abd4ab
= <=>
2026-05-11 12:45:02 +0200

    instalar y servir bootstrap

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-import express from "express"
-import devs from "./data/devs.json" with { type: "json" }
+import express from 'express'
+import devs from './data/devs.json' with { type: 'json' }
+import path from 'path'
 
 const PORT = 3000
 const app = express()
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+// Servir los archivos de Bootstrap como estáticos
+app.use('/css/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
 app.get('/', (req, res) => {
   res.render('index')
